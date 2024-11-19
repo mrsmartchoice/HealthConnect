78fe6486c12abfc7e9ee45436f6e3a1874b66f80
mrsmartchoice <technicaleldnight@gmail.comgit config --global user.email technicaleldnight@gmail.com>
2024-11-19 23:40:47 +0100

    server upto date with env

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import healthAlertRoutes from './routes/healthAlert.js';
 import healthCampaignsRoutes from './routes/healthCampaigns.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
